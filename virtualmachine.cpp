3f694cafb35d091e7c70c3d2aa7937ebc6c085a8
Areen <areenasiri1212@gmail.com>
2025-06-21 21:49:22 +0800

    Added ADD and SUB arthimetic instruction

--- virtualmachine.cpp
+++ virtualmachine.cpp
@@ -6,12 +6,18 @@ using namespace std;
 
 int main () {
     cout << "Virtual Machine Simulator" << endl;
-    int registers[8] = {0}; 
+    char registers[8] = {0}; 
 
     registers[0] = 5; 
-    cout << "After MOV 5 , R0: R0 =" << registers[0] << endl;
+    cout << "After MOV 5 , R0: R0 =" << (int) registers[0] << endl;
 
     registers[1] = 10;
-    cout << "After MOV 10 , R1 : R1 = " << registers[1] << endl;
+    cout << "After MOV 10 , R1 : R1 = " << (int) registers[1] << endl;
     
+    registers[0] = registers[0] + registers[1];
+    cout << "After ADD R1, R0: R0 =" << (int) registers[0] << endl;
+
+    registers[0] = registers[0] - 3; 
+    cout << "After SUB 3, R0: R0 = " << (int) registers[0] << endl;
+
 }
