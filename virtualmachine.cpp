debd50ebfd71db1a637d4b7b1eefd39ee655e656
Areen <areenasiri1212@gmail.com>
2025-06-21 22:16:30 +0800

    Implemented INC instruction

--- virtualmachine.cpp
+++ virtualmachine.cpp
@@ -4,20 +4,34 @@
 #include <vector>
 using namespace std;
 
-int main () {
+int main()
+{
     cout << "Virtual Machine Simulator" << endl;
-    char registers[8] = {0}; 
+    char registers[8] = {0};
 
-    registers[0] = 5; 
-    cout << "After MOV 5 , R0: R0 =" << (int) registers[0] << endl;
+    registers[0] = 5;
+    cout << "After MOV 5 , R0: R0 =" << (int)registers[0] << endl;
 
     registers[1] = 10;
-    cout << "After MOV 10 , R1 : R1 = " << (int) registers[1] << endl;
-    
+    cout << "After MOV 10 , R1 : R1 = " << (int)registers[1] << endl;
+
     registers[0] = registers[0] + registers[1];
-    cout << "After ADD R1, R0: R0 =" << (int) registers[0] << endl;
+    cout << "After ADD R1, R0: R0 =" <<(int)registers[0] << endl;
 
-    registers[0] = registers[0] - 3; 
-    cout << "After SUB 3, R0: R0 = " << (int) registers[0] << endl;
+    registers[0] = registers[0] - 3;
+    cout << "After SUB 3, R0: R0 = " <<(int)registers[0] << endl;
 
+    registers[0] = registers[0] * 2;
+    cout << "After MUL 2 , R0 : R0 = "<<(int)registers[0] << endl;
+
+    registers[0] = registers[0] / 3;
+    cout << "After DIV 3 , R0 : R0 = "<<(int)registers[0] << endl;
+
+    registers[0] = registers[0] + 1;
+    cout << "After INC R0 : R0 = " <<(int)registers[0] << endl;
+
+    registers[0] = registers[0] - 1;
+    cout << "After DEC R0 : R0 = " <<(int)registers[0] << endl;
+
+    return 0;
 }
