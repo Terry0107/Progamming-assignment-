1fa73997e16ef9846f31b3425880e22cf7b8c06a
Areen <areenasiri1212@gmail.com>
2025-06-24 12:49:02 +0800

    all the addition and the changes i made also added executesub and the spaces too and mul as well and the executeadd and also deleted some lines

--- virtualmachine.cpp
+++ virtualmachine.cpp
@@ -46,9 +46,26 @@ using namespace std;
  void executeSUB(char registers[] ){
  registers[0] = registers[0] - 3; 
     cout << "After SUB 3, R0 : R0 =" << (int) registers[0] << endl; 
+    char operand1 = registers[0];
+    char operand2_val = 3;
+    int result_int = (int)operand1 - (int)operand2_val;
+    
+    registers[0] = (char)result_int;
+    cout << "After SUB 3, R0 : R0 = " << (int) registers[0] << endl; 
+
+    ZF = (registers[0] == 0); 
+
+    OF = ((operand1 > 0 && operand2_val < 0 && registers[0] < 0 ) ||
+       (operand1 < 0 && operand2_val > 0 && registers[0] > 0 )); 
+    OF = (operand1 > 0 && result_int < -128) || 
+       (operand1 < 0 && result_int > 127 );
+    
+    UF = (operand1 > 0 && registers[0] < 0 && (operand1 - operand2_val) < -128 );
+    CF = ((unsigned char )operand1 < (unsigned char )operand2_val);
+    displayFlags();
  }
 
- void executeSHL(char registers []) {
+ void executeSHL(char registers []){
     char original_r0 = registers[0];
     registers[0] = registers[0] << 1; 
 
@@ -61,9 +78,9 @@ using namespace std;
      OF = (((original_r0 & 0x40) >>6) != ((registers[0] & 0x80) >>7 ));
      UF = false;
      displayFlags();
-      }
+     }
 
-     void executeSHR(char registers[]) {
+     void executeSHR(char registers[]){
         char original_r0 = registers[0]; 
         registers[0] = registers[0] >> 1;
          cout << "After SHR R0, 1 : R0 = " << (int) registers[0] << endl; 
@@ -75,7 +92,7 @@ using namespace std;
          displayFlags();
  }
 
- void executeROL (char registers []) {
+ void executeROL(char registers []){
     unsigned char temp_val_rol = (unsigned char ) registers[0];
     unsigned char wrapped_bit_rol = (temp_val_rol >> 7 ) & 1; 
     temp_val_rol = temp_val_rol << 1; 
@@ -105,12 +122,52 @@ using namespace std;
     displayFlags();
  }
 
+ void executeMUL(char registers[]){
+    char operand1 = registers[0]; 
+    char operand2_val = 2;
+
+    int result_int = (int)operand1 * (int)operand2_val;
+    registers[0] = (char)result_int;
+
+     cout << "After MUL 2 , R0 : R0 =" << (int)registers[0] << endl; 
+
+     ZF = (registers[0] ==0); 
+     OF = (result_int > 127 || resutlt_int < -128);
+     UF = (operand1 < 0 && operand2_val > 0 && registers[0] > 0); 
+     CF = (((unsigned char )operand1 *(unsigned char)operand2_val) > 255);
+    displayFlags();
+
+ }
+
+ void executeDIV(char registers[]){
+    char operand1 = registers[0]; 
+    char operand2_val = 3; 
+    
+    if (operand2_val == 0){
+        cout << "Error: Divison by zero!" << endl; 
+      ZF = true; 
+      OF = false;
+      UF = false; 
+      CF = false; 
+      displayFlags();
+      return 0; 
+    }
+    int result_int = (int)operand1 / (int)operand2_val;
+    registers[0] = (char)result_int; 
+
+       cout << "After DIV 3, R0 : R0 =" << (int)registers[0] << endl; 
+      ZF = (registers[0] == 0);
+      OF = false;
+      UF = false;
+      CF = false; 
+      displayFlags();
+}
+ 
 
 int main(){
-
     cout << "Virtual Machine Simulator" << endl;
-    char registers[8] = {0};
     
+    char registers[8] = {0};
     displayFlags();
 
     registers[0] = 5;
@@ -120,29 +177,50 @@ int main(){
     cout << "After MOV 10 , R1 : R1 = " << (int)registers[1] << endl;
 
  executeADD (registers); 
+
  registers[0] = 120; 
  registers[1] = 20; 
  cout << "\n--- Testing ADD with potential POSITIVE OVERFLOW --- \n";
  executeADD (registers); 
+
  registers[0] = -5;
  registers[1] = 5;
   cout << "\n--- Testing ADD with ZERO result --- \n"; 
  executeADD (registers); 
+
    registers[0] = -120; 
    registers[1] = -20;
    cout << "\n--- Testing ADD with potential NEGATIVE OVERFLOW ( UNDERFLOW) ---\n";
  executeADD (registers); 
+
  registers[0] = 15; 
- cout << "\nAfter restting R0 to 15\n";
+ cout << "\n--- Testing SUB (R0 = 15, SUB 3) ---\n";
+ executeSUB(registers);
 
- displayFlags();
+ registers[0] = 0; 
+  cout << "\n--- Testing SUB (R0=0, SUB 3) ---\n ";
+  executeSUB(registers);
+
+  registers[0] = -120;
+  cout << "\n--- Testing SUB (R0=-127, SUB 3) ---\n ";
+  executeSUB(registers);
+
  
- executeSUB (registers);
+ registers[0] = 50; 
+    cout << "\n--- Testing MUL (R0=50, MUL 2) ---\n";
+    executeMUL(registers);
 
+ registers[0] = 70;
+    cout << "\n--- Testing MUL (R0=70, MUL 2)---(Positive Overflow)\n";
+    executeMUL(registers);
 
- 
- registers[0] = registers[0] * 2;
-    cout << "After MUL 2 , R0 : R0 = "<<(int)registers[0] << endl;
+ registers[0] = -70; 
+    cout << "\n--- Testing MUL (R0= -70, MUL 2)(---\n";
+    executeMUL(registers);
+
+ registers[0] = 0; 
+    cout << "\n--- Testing MUL (R0=0, MUL 2)"
+    
 
  registers[0] = registers[0] / 3;
     cout << "After DIV 3 , R0 : R0 = "<<(int)registers[0] << endl;
@@ -153,46 +231,55 @@ int main(){
  registers[0] = registers[0] - 1;
     cout << "After DEC R0 : R0 = " <<(int)registers[0] << endl;
 
- executeSHL(registers);  
+
+ cout << "\n--- Testing SHL (R0=5) ---\n"; 
  registers[0] = 5; 
-  cout << "\n--- Testing SHL (R0=5) ---\n";
  executeSHL(registers); 
+
+ cout << "\n--- Testing SHL (R0= 64, POSITIVE overflow ) ---\n";
  registers[0] = 64; 
-  cout << "\n--- Testing SHL (R0= 64, POSITIVE overflow ) ---\n";
  executeSHL(registers); 
+
+ cout << "\n--- Testing SHL (R0= -64, negative overflow ) ---\n";
  registers[0] = -64; 
-  cout << "\n--- Testing SHL (R0= -64, negative overflow ) ---\n";
  executeSHL(registers);
- registers[0] = 10; 
+
+
  cout <<  "\n--- Testing SHR (R0= 10) ---\n";
- executeSHR(registers);
- registers[0] = 7;
- cout << "\n--- Testing SHR (R0=7) ---\n";
- executeSHR(registers);
- registers[0] = 5; 
- cout << "\n--- Testing ROL (R0=5) ---\n";
- executeROL(registers);
- registers[0] = -128; 
- cout << "\n--- Testing ROL (R0=-128) ---\n";
- executeROL(registers);
  registers[0] = 10; 
+ executeSHR(registers);
+
+ cout << "\n--- Testing SHR (R0=7) ---\n";
+ registers[0] = 7;
+ executeSHR(registers);
+
+ cout << "\n--- Testing ROL (R0=5) ---\n";
+ registers[0] = 5; 
+ executeROL(registers);
+
+
+ cout << "\n--- Testing ROL (R0=-128) ---\n";
+ registers[0] = -128; 
+ executeROL(registers);
+
+
  cout << "\n--- Testing ROR (R0=10) --- \n";
+ registers[0] = 10; 
  executeROR(registers);
- registers[0] = 1; 
+
  cout << "\n--- Testing ROR (R0=1)--- \n";
+ registers[0] = 1; 
  executeROR(registers); 
 
 
  char input_char; 
   cout << "Input for R0 (enter a single digit or character) : ?"; 
   cin >> input_char; 
-
  registers[0] = input_char; 
    cout << "After Input R0 : R0 = " << (int) registers[0] << " ( ASCII value)" << endl; 
 
    cout << "After OUT R0 : Displaying R0 value = " << (int) registers[0] << endl; 
 
     return 0;
-
 }
  
